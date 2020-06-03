1fa13efd6d50cada3a2332bf722a2a379c56e4f2
ecoello95 <51451025+ecoello95@users.noreply.github.com>
2020-06-03 01:03:19 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -10,11 +10,11 @@ echo "<h1>".$_GET['numero']."</h1>";
 
 if(is_numeric($_GET['numero']) && $_GET['numero'] >=1){
     if($_GET['numero']%2!=0){
-        echo "<h2>"."Es un numero primo".$_GET['numero']."</h2>";
+        echo "<h2>"."Es un numero primo"."</h2>";
     }
 
     else{
-        echo "<h2>"."No un numero primo".$_GET['numero']."</h2>";
+        echo "<h2>"."No un numero primo"."</h2>";
     }
 
   
