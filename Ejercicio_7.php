33624c076fc3711b66fbc3ee83c564f1762e4811
ecoello95 <51451025+ecoello95@users.noreply.github.com>
2020-06-03 09:51:30 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -1,26 +1,44 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
+    <h2>Ejercicio 7</h2>
+    <p>Programa que evalua si un numero ingresado es un numero primo.</p>
+    <form action="ejercicio7.php" method="post">
+        Ingrese un numero: <input type="number" name="txt1">
+        <br><br>
+        <input type="submit" name="primo">
+    </form>
+</body>
+</html>
 
-<h1> Ejercicio #7 Programa que evalua si un numero ingresado es un numero primo </h1>
 <?php
-	header ("content-type: text/html;charset=\"utf-8\"");
 
-echo "<h1>".$_GET['numero']."</h1>";
+    if(isset($_POST["primo"])){
+        $n=$_POST["txt1"];
+        for($i=2;$i<=$n/2;$i++){
+            if($n % $i ==0)
+            {
+                $f=1;
+                break;
+            }
 
-if(is_numeric($_GET['numero']) && $_GET['numero'] >=1){
-    if($_GET['numero']%2!=0){
-        echo "<h2>"."Es un numero primo"."</h2>";
+        }
+        if($f==0)
+        {
+            echo "$n Es un numero primo";
+        }
+        else
+        {
+            echo "$n No es un numero primo";
+        }
+
+
     }
-
-    else{
-        echo "<h2>"."No un numero primo"."</h2>";
-    }
-
-  
-}
-
-
 ?>
 
 <form>
